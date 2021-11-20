badaca799dd707769fdb8c589e6f6d3cefe84a35
kuba-budzynski <main.kubabudzynski@gmail.com>
2021-11-20 21:03:46 +0100

    Prefix to css selectors

--- webpack.js
+++ webpack.js
@@ -12,139 +12,136 @@ const ora = require("ora");
 const safeDirectoryRemove =
   require("./src/utils/fileUtils").safeDirectoryRemove;
 
-const compiler = webpack({
-  mode: "development",
-  devtool: "source-map",
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      "process.env.NODE_ENV": "development",
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.join(
-            __dirname,
-            "src",
-            "build",
-            "plugin",
-            "logic",
-            "server.js"
-          ),
-          to: path.join(__dirname, "dist", "server.js"),
-        },
-      ],
-      options: {
-        concurrency: 100,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-      chunkFilename: "styles.css",
-    }),
-  ],
-  entry: {
-    main: path.join(__dirname, "src", "build", "index.js"),
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    libraryTarget: "commonjs",
-    filename: "entry.js",
-  },
-  externals: {
-    ...Object.keys(remoteComponentConfig).reduce(
-      (obj, key) => ({ ...obj, [key]: key }),
-      {}
-    ),
-    "remote-component.config.js": "remote-component.config.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|mjs|jsx|ts|tsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // {
-          //   loader: "style-loader",
-          // },
+async function build(id) {
+  const config = {
+    mode: "development",
+    devtool: "source-map",
+    plugins: [
+      new webpack.EnvironmentPlugin({
+        "process.env.NODE_ENV": "development",
+      }),
+      new CopyPlugin({
+        patterns: [
           {
-            loader: "css-loader",
+            from: path.join(
+              __dirname,
+              "src",
+              "build",
+              "plugin",
+              "logic",
+              "server.js"
+            ),
+            to: path.join(__dirname, "dist", "server.js"),
           },
-          {
-            loader: require.resolve("postcss-loader"),
-            options: {
-              postcssOptions: {
-                plugins: {
-                  "postcss-prefix-selector": {
-                    prefix: ".my-prefix",
-                    transform(prefix, selector, prefixedSelector, filepath) {
-                      if (selector.match(/^(html|body)/)) {
-                        return selector.replace(/^([^\s]*)/, `$1 ${prefix}`);
-                      }
-
-                      if (filepath.match(/node_modules/)) {
-                        return selector; // Do not prefix styles imported from node_modules
-                      }
-
-                      return prefixedSelector;
+        ],
+        options: {
+          concurrency: 100,
+        },
+      }),
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+        chunkFilename: "styles.css",
+      }),
+    ],
+    entry: {
+      main: path.join(__dirname, "src", "build", "index.js"),
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      libraryTarget: "commonjs",
+      filename: "entry.js",
+    },
+    externals: {
+      ...Object.keys(remoteComponentConfig).reduce(
+        (obj, key) => ({ ...obj, [key]: key }),
+        {}
+      ),
+      "remote-component.config.js": "remote-component.config.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|mjs|jsx|ts|tsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // {
+            //   loader: "style-loader",
+            // },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: require.resolve("postcss-loader"),
+              options: {
+                postcssOptions: {
+                  plugins: {
+                    "postcss-prefix-selector": {
+                      prefix: `.${id}`,
+                      transform(prefix, selector, prefixedSelector, filepath) {
+                        if (filepath.match(/node_modules/)) {
+                          return selector;
+                        }
+                        return prefixedSelector;
+                      },
                     },
+                    autoprefixer: {},
                   },
-                  autoprefixer: {},
                 },
               },
             },
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ["file-loader"],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(csv|tsv)$/i,
+          use: ["csv-loader"],
+        },
+        {
+          test: /\.xml$/i,
+          use: ["xml-loader"],
+        },
+        {
+          test: /\.toml$/i,
+          type: "json",
+          parser: {
+            parse: toml.parse,
           },
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
         },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
+        {
+          test: /\.yaml$/i,
+          type: "json",
+          parser: {
+            parse: yaml.parse,
+          },
         },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
+        {
+          test: /\.json5$/i,
+          type: "json",
+          parser: {
+            parse: json5.parse,
+          },
         },
-      },
-    ],
-  },
-});
+      ],
+    },
+  };
 
-async function build() {
+  const compiler = webpack(config);
+
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
       compiler.close(() => {
@@ -165,7 +162,7 @@ async function runBuild(plugin) {
   safeDirectoryRemove(p);
 
   try {
-    const res = await build();
+    const res = await build(plugin.requestId);
     if (res.success) {
       buildSpinner.succeed();
     } else {
