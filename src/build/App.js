4f8575049092fe843409594d424e2bf52dfb4ce5
Karol Sitarz <ka.sitarz@gmail.com>
2021-11-20 18:25:00 +0100

    add users to plugin build

--- src/build/App.js
+++ src/build/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Plugin from "./plugin/components/client.jsx";
 
-export const App = ({ websockets, ids, size }) => {
-  return <Plugin websockets={websockets} ids={ids} size={size} />;
+export const App = ({ websockets, ids, size, users }) => {
+  return <Plugin {...{ websockets, ids, size, users }} />;
 };
