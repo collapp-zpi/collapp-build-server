06972c69c298462e7f86a3e6ca2415237c8d6939
kuba-budzynski <main.kubabudzynski@gmail.com>
2021-11-21 01:23:44 +0100

    Add logs back

--- src/utils/updateDB.ts
+++ src/utils/updateDB.ts
@@ -11,9 +11,9 @@ export async function successBuild(plugin: PluginRequest) {
       data: {
         isBuilding: false,
         isPending: false,
-        // logs: {
-        //   create: [{ content: "Build success" }],
-        // },
+        logs: {
+          create: [{ content: "Build success" }],
+        },
       },
     });
     const draft = await prisma.draftPlugin.findUnique({
@@ -29,8 +29,8 @@ export async function successBuild(plugin: PluginRequest) {
     });
     if (draft?.published) {
       await prisma.file.delete({
-        where: { publishedId: plugin.requestId }
-      })
+        where: { publishedId: plugin.requestId },
+      });
     }
     const updatePublished = await prisma.publishedPlugin.upsert({
       where: {
@@ -84,9 +84,9 @@ export async function failBuild(plugin: PluginRequest) {
       data: {
         isBuilding: false,
         isPending: false,
-        // logs: {
-        //   create: [{ content: 'Build error' }],
-        // },
+        logs: {
+          create: [{ content: "Build error" }],
+        },
       },
     });
     return {
